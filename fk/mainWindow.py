7ab7274a5ac31c247657ae244bb1f6d226955d0b
EightBitBoy <haertel.dev@gmail.com>
2012-06-20 02:36:52 +0200

    add a menu bar

--- fk/mainWindow.py
+++ fk/mainWindow.py
@@ -3,4 +3,11 @@ import wx
 class MainWindow(wx.Frame):
 	def __init__(self, parent, title):
 		wx.Frame.__init__(self, parent, wx.ID_ANY ,title)
+		
+		fennekMenu = wx.Menu()
+		fennekMenu.Append(wx.ID_EXIT, "Settings", "Fennek settings")
+		menuBar = wx.MenuBar()
+		menuBar.Append(fennekMenu, "Fennek")
+		self.SetMenuBar(menuBar)
+		
 		self.Show(True)
