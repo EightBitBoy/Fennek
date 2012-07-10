751a77406d120c9f8d34904c3ebd84da020d1937
EightBitBoy <haertel.dev@gmail.com>
2012-07-11 01:08:46 +0200

    config has a default section

---
+++ fk/config.py
@@ -0,0 +1,2 @@
+class default:
+	i = 0
--- fk/configurator.py
+++ fk/configurator.py
@@ -1,5 +1,14 @@
+import config
+
 def updateConfig():
 	checkForExistingConfigFile()
+	print config.default.i
 
 def checkForExistingConfigFile():
+	pass
+
+def loadConfig():
+	pass
+
+def writeConfig():
 	pass
